9ff2d7b6ae7c32e9f83315b341130a5310be6adf
LG <813051286@qq.com>
2020-12-19 19:41:14 +0800

    first commit

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,20 @@
+package com.tensquare.base;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import util.IdWorker;
+
+@SpringBootApplication
+@CrossOrigin
+public class BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class,args);
+    }
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker();
+    }
+}
---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -0,0 +1,45 @@
+package com.tensquare.base.controller;
+
+import com.tensquare.base.pojo.Label;
+import com.tensquare.base.service.LabelService;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/label")
+public class LabelController {
+    @Autowired
+    private LabelService labelService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(true, StatusCode.OK, "查询成功",labelService.finAll());
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.GET)
+    public Result findById(@PathVariable("labelId") String labelId) {
+        return new Result(true, StatusCode.OK, "查询成功",labelService.findById(labelId));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Result save(@RequestBody Label label) {
+        labelService.save(label);
+        return new Result(true, StatusCode.OK, "添加成功");
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.PUT)
+    public Result alterById(@PathVariable("labelId") String labelId, @RequestBody Label label) {
+        label.setId(labelId);
+        labelService.update(label);
+        return new Result(true, StatusCode.OK, "更新成功");
+    }
+
+    @RequestMapping(value = "/{labelId}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable("labelId") String labelId) {
+        labelService.deleteById(labelId);
+        return new Result(true, StatusCode.OK, "删除成功");
+    }
+}
